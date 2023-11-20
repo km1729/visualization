d0fd2b2292851f7a37766048a28edcc370052ada
kmcin <kellymcintyre@outlook.com.au>
2023-11-20 22:24:25 +1100

    update vega js internal link

--- vega/js/line.js
+++ vega/js/line.js
@@ -1,12 +1,12 @@
 const yourSpec = {
     "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
     "description": "Your Vega-Lite Chart",
-    "mark": "bar",
+    "mark": "line",
     "encoding": {
         "x": {"field": "category", "type": "ordinal"},
         "y": {"field": "value", "type": "quantitative"}
     },
-    "data": {"url": "/home/k/projects/visualization/Vega/data/line_chart.json"} // Replace with your data source
+    "data": {"url": "data/line_chart.json"} // Corrected path to your JSON file
 };
 
 vegaEmbed("#vis", yourSpec);
